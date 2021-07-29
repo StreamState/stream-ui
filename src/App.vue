a9969c2db419d3ce7da969b37ed20803a4c1a8e2
thomasnguyen704 <thomasnguyen704@gmail.com>
2021-07-28 22:08:07 -0400

    added blank footer

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,21 @@
   <div>
     <Navbar />
     <router-view />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar.vue";
+import Footer from "@/components/Footer.vue";
+
 // eslint-disable-next-line no-unused-vars
 
 export default {
   name: "App",
   components: {
-    Navbar,
+    Navbar, 
+    Footer
   },
   data: function () {
     return {};
---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer>
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  footer {
+    height: 25vh;
+  }
+</style>
